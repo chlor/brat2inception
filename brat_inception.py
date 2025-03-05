bfbd1d21a58211c35e0b3884aa593e6d832b2a65
chlor <christina.lohr@uni-jena.de>
2025-03-05 20:32:58 +0100

    Update semann_layer.

--- brat_inception.py
+++ brat_inception.py
@@ -3,17 +3,33 @@ from cassis import *
 from cassis.typesystem import TYPE_NAME_FS_ARRAY, TYPE_NAME_ANNOTATION
 
 
-typesystem = TypeSystem()
-neg_fact = typesystem.create_type(name='example.Negation_Faktuality')
-typesystem.create_feature(domainType=neg_fact, name='example', rangeType=TYPE_NAME_ANNOTATION)
-cas = Cas(typesystem=typesystem)
+#typesystem = TypeSystem()
+#neg_fact = typesystem.create_type(name='example.Negation_Faktuality')
+#typesystem.create_feature(domainType=neg_fact, name='example', rangeType=TYPE_NAME_ANNOTATION)
+
+
+#with open('FactCharact_entities_layer.xml', 'rb') as f:
+with open('TypeSystem_semant_Ann.xml', 'rb') as f:
+    typesystem = load_typesystem(f)
+
+print(typesystem.get_types())
+
+#for t in typesystem.get_types():
+#    print(t)
+
+
+
 
 anno_file = '/home/chlor/PycharmProjects/brat2inception/test_data/Albers.ann'
 text_file = '/home/chlor/PycharmProjects/brat2inception/test_data/Albers.txt'
 
 plain_text = open(text_file, "r", encoding="utf-8").read()
 
-cas.sofa_string = plain_text
+cas = Cas(
+    typesystem=typesystem,
+    sofa_string=plain_text,  # Text
+    document_language='de'
+)
 
 ann = pd.read_csv(anno_file, delimiter='\t', index_col=0, header=None)
 ann.columns = ['entity_type_begin_end', 'text']
@@ -28,9 +44,14 @@ for index, line in ann.iterrows():
         begin = spl[1]
         end = spl[2]
 
-        Annotation = cas.typesystem.get_type(TYPE_NAME_ANNOTATION)
-        new_ann = Annotation(begin=int(begin), end=int(end))#, text=line['text'])
-
-        cas.add_annotation(new_ann)
+        Token = typesystem.get_type('gemtex.Concept')
+        cas.add(
+            Token(
+                begin=int(begin),
+                end=int(end),
+                id=spl[0],
+                literal=str(spl[0])
+            )
+        )
 
 cas.to_json('test_data/new_cas.json')
